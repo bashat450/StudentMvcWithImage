0b0aa94bd8d9a7b28b45524f5f2178423a330a17
Bashat Parween <68892857+bashat450@users.noreply.github.com>
2025-05-09 12:02:09 +0530

    Add files via upload

---
+++ Logic.cs
@@ -0,0 +1,131 @@
+﻿using StudentsCRUDWithImageMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace StudentsCRUDWithImageMVC
+{
+    public class Logic
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["dbStd"].ToString());
+
+        #region === Display All Details ===
+        public List<ListsModel> GetAllStudentsDetails()
+        {
+            List<ListsModel> LMD = new List<ListsModel>();
+            try
+            {
+                SqlCommand cmd = new SqlCommand("[dbo].[GetAllStudentsDetails_SP]", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlDataAdapter SDA = new SqlDataAdapter(cmd);
+                DataTable DT = new DataTable();
+                SDA.Fill(DT);
+
+                foreach (DataRow row in DT.Rows)
+                {
+                    ListsModel std = new ListsModel
+                    {
+                        RollNo = Convert.ToInt32(row["RollNo"]),
+                        Name = row["Name"].ToString(),
+                        City = row["City"].ToString(),
+                        State = row["State"].ToString(),
+                        Fees = Convert.ToInt32(row["Fees"]),
+                        JoiningDate = Convert.ToDateTime(row["JoiningDate"]),
+                        Image = row["Image"] == DBNull.Value ? null : (byte[])row["Image"]
+                    };
+                    LMD.Add(std);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log or handle error
+                Console.WriteLine("Error in row : " + ex.Message);
+            }
+            return LMD;
+        }
+        #endregion
+
+        #region === insert Student Logic ===
+        public void insertDetails(InsertModel insertStdValues)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("[dbo].[InsertValuesInStudents_SP]", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@Name", insertStdValues.Name);
+                cmd.Parameters.AddWithValue("@City", insertStdValues.City);
+                cmd.Parameters.AddWithValue("@State", insertStdValues.State);
+                cmd.Parameters.AddWithValue("@Fees", insertStdValues.Fees);
+                cmd.Parameters.AddWithValue("@JoiningDate", insertStdValues.JoiningDate);
+                cmd.Parameters.AddWithValue("@Image", insertStdValues.Image ?? (object)DBNull.Value);
+
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        #endregion
+
+        #region === Edit Student Details ===
+        public void EditDetails(EditModel EditStdValues)
+        {
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("[dbo].[UpadateStudentValues_SP]", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@RollNo", EditStdValues.RollNo);
+                cmd.Parameters.AddWithValue("@Name", EditStdValues.Name);
+                cmd.Parameters.AddWithValue("@City", EditStdValues.City);
+                cmd.Parameters.AddWithValue("@State", EditStdValues.State);
+                cmd.Parameters.AddWithValue("@Fees", EditStdValues.Fees);
+                cmd.Parameters.AddWithValue("@JoiningDate", EditStdValues.JoiningDate);
+                cmd.Parameters.AddWithValue("@Image", EditStdValues.Image ?? (object)DBNull.Value);
+
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+        #endregion
+
+        #region === Delete Student Details ===
+        public void DeleteDetails(int id)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("[dbo].[DeleteStudentValues_SP]", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@RollNo", id);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        #endregion
+    }
+
+}
---
+++ StudentsController.cs
@@ -0,0 +1,109 @@
+﻿using StudentsCRUDWithImageMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace StudentsCRUDWithImageMVC.Controllers
+{
+    public class StudentsController : Controller
+    {
+        Logic _logic = new Logic();
+        // GET:                            Lists Of Students
+        [HttpGet]
+        public ActionResult Lists()
+        {
+            return View(_logic.GetAllStudentsDetails());
+        }
+
+        // GET:                     GetDetails     Students/Details/5
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            var details = _logic.GetAllStudentsDetails().Find(de => de.RollNo == id);
+            return View(details);
+        }
+
+        // GET: Students/Create
+        [HttpGet]
+        public ActionResult Insert()
+        {
+            return View();
+        }
+
+        // POST: Students/Create
+        [HttpPost]
+        public ActionResult Insert(InsertModel insertStdValues)
+        {
+            try
+            {
+                if (insertStdValues.ImageFile != null && insertStdValues.ImageFile.ContentLength > 0)
+                {
+                    using (var binaryReader = new BinaryReader(insertStdValues.ImageFile.InputStream))
+                    {
+                        insertStdValues.Image = binaryReader.ReadBytes(insertStdValues.ImageFile.ContentLength);
+                    }
+                }
+
+                _logic.insertDetails(insertStdValues); // Save data
+
+                return RedirectToAction("Lists"); // Redirect to Lists after save
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Students/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var students = _logic.GetAllStudentsDetails().Find(s => s.RollNo == id);
+            return View(students);
+        }
+
+        // POST: Students/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, EditModel EditStdValues)
+        {
+            try
+            {
+                if (EditStdValues.ImageFile != null && EditStdValues.ImageFile.ContentLength > 0)
+                {
+                    using (var binaryReader = new BinaryReader(EditStdValues.ImageFile.InputStream))
+                    {
+                        EditStdValues.Image = binaryReader.ReadBytes(EditStdValues.ImageFile.ContentLength);
+                    }
+                }
+                // TODO: Add update logic here
+                _logic.EditDetails(EditStdValues);
+
+                return RedirectToAction("Lists");
+            }
+            catch
+            {
+                return View(EditStdValues);
+            }
+            
+        }
+
+        //GET: Students/Delete/5
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var del = _logic.GetAllStudentsDetails().Find(de => de.RollNo == id);
+            return View(del);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            _logic.DeleteDetails(id);
+            return RedirectToAction("Lists");
+        }
+    }
+}
